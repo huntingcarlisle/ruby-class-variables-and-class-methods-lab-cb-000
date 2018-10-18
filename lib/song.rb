c5a3a193f59e9ea99365ac6840dbcadc41f9f940
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-18 21:24:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,7 @@ class Song
   end
   
   def self.genre_count
-    @@genres.uniq.map {|genre| {genre: @@genres.count(genre)}
+    @@genres.uniq.map {|genre| {genre: @@genres.count(genre)}}
   end
   
   def self.artist_count
