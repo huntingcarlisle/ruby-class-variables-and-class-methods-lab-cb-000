3265a2c701e710a5a9a93694ab02c0736ca32a01
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-18 21:04:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,21 @@
 class Song
   
   attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
+    @@artists << artist
     @genre = genre
+    @@genres << genre
+    @@count++
+  end
+  
+  def self.count
+    return @@count
   end
   
   
