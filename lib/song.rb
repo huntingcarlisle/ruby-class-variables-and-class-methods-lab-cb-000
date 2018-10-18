b41f3b1dd1c6aa9ea95b2599cc64fbe884bd2cf0
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-18 21:13:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
-    @@artists << artist 
+    @@artists << artist unless @@artists.include?(artist)
     @genre = genre
     @@genres << genre unless @@genres.include?(genre)
     @@count += 1
